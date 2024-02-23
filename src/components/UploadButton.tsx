e7124827b5dc95730d23c4cb4c69ae47bb80124d
Julio Condor <julioc3bgud@gmail.com>
2024-02-22 22:23:00 -0500

    Add upload button

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>
+          Subir PDF
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        test
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
