a4647d5ee74dfc3bcaae122d257c3c8c35124f96
Julio Condor <julioc3bgud@gmail.com>
2024-02-22 22:25:45 -0500

    Add on dashboard page - redirect for not db user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -8,7 +10,17 @@ const Page = async () => {
 
   if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  })
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+
+
+  return <Dashboard/>;
 };
 
 export default Page;
