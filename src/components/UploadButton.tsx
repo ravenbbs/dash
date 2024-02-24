c92e61fcefcfe3237c6f7496c1b9bd5bcd94e7a3
Julio Condor <julioc3bgud@gmail.com>
2024-02-23 23:19:17 -0500

    Add config on upload button - drag and drop

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,122 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+import { Cloud, File } from "lucide-react";
+import { Progress } from "./ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
+
+const UploadDropzone = () => {
+  const router = useRouter()
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const {toast} = useToast()
+
+  const { startUpload } = useUploadThing("pdfUploader")
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`)
+    },
+    retry: true,
+    retryDelay: 500
+  })
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        return prevProgress + 5;
+      });
+    }, 500);
+    return interval
+  };
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+
+        const progressInterval = startSimulatedProgress()
+
+        const res = await startUpload(acceptedFile)
+
+        if(!res) {
+          return toast({
+            title: "Ocurrió un error",
+            description: "Por favor intenta de nuevo",
+            variant: "destructive"
+          })
+        }
+
+        const [fileResponse] = res
+
+        const key = fileResponse?.key
+
+        if(!key) {
+          return toast({
+            title: "Ocurrió un error",
+            description: "Por favor intenta de nuevo",
+            variant: "destructive"
+          })
+        }
+
+        clearInterval(progressInterval)
+        setUploadProgress(100)
+
+        startPolling({key})
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer transition duration-300 bg-gray-50 hover:bg-gray-100 "
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-8 w-8 text-zinc-700 mb-2" />
+                <p className="mb-2 text-sm text-zinc-600">
+                  <span className="font-semibold text-zinc-900 ">
+                    Click para cargar un PDF
+                  </span>{" "}
+                  o arrastra y suelta
+                </p>
+                <p className="text-xs text-zinc-500">PDF hasta (4 MB)</p>
+              </div>
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-6 w-6 text-blue-600" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200" />
+                </div>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -17,12 +133,10 @@ const UploadButton = () => {
       }}
     >
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-        <Button>
-          Subir PDF
-        </Button>
+        <Button>Subir PDF</Button>
       </DialogTrigger>
       <DialogContent>
-        test
+        <UploadDropzone />
       </DialogContent>
     </Dialog>
   );
