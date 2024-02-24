5fe3a6c5412bf16d0250166d5a81e1032376703f
Julio Condor <julioc3bgud@gmail.com>
2024-02-23 23:15:14 -0500

    Add uploadthing files - core - route

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,20 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
