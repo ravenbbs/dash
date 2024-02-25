b9e688b720c67c6158215872eac39e64f649cd94
Julio Condor <julioc3bgud@gmail.com>
2024-02-24 19:15:17 -0500

    Change next config - webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, idServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
 export default nextConfig;
