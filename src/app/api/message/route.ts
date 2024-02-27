20d6d3adedee53837f2d72b6b4c675fe7d984a8d
Julio Condor <julioc3bgud@gmail.com>
2024-02-27 18:23:29 -0500

    Add api message route

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+import { NextRequest } from "next/server";
+
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json()
+
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const userId = user?.id
+  if (!userId)
+  return new Response('Unauthorized', {status: 401})
+
+  const {fileId, message} = SendMessageValidator.parse(body)
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId
+    }
+  })
+
+  if (!file)
+  return new Response('Not found', {status: 404})
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId
+    }
+  })
+
+}
