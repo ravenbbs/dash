999cb883e3df4f8b19932d14f94ad189efefc391
Julio Condor <julioc3bgud@gmail.com>
2024-02-28 00:04:41 -0500

    Add connect with pinecone & open ai

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,13 +1,17 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { PineconeStore } from "@langchain/pinecone";
+import { getPineconeClient } from "@/lib/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
+    .middleware(async () => {
       const { getUser } = getKindeServerSession();
       const user = await getUser();
 
@@ -16,16 +20,64 @@ export const ourFileRouter = {
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-
       const createdFile = await db.file.create({
         data: {
           key: file.key,
           name: file.name,
           userId: metadata.userId,
           url: `https://utfs.io/f/${file.key}`,
-          uploadStatus: 'PROCESSING'
+          uploadStatus: "PROCESSING",
+        },
+      });
+
+      try {
+        const response = await fetch(`https://utfs.io/f/${file.key}`);
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load();
+        const pagesAmt = pageLevelDocs.length;
+
+        const pc = new Pinecone({
+          apiKey: process.env.PINECONE_API_KEY!
+        });
+        const pineconeIndex = pc.index('quickstart');
+
+        //const client = await getPineconeClient();
+        //const pineconeIndex = await client.index("dash");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs,embeddings, 
+          {
+          pineconeIndex,
+          namespace: createdFile.id,
         }
-      })
+        );
+        console.log(file.url)
+        console.log(pageLevelDocs + '-----------Page level docs')
+        console.log(pineconeIndex + 'Pinecone index ----------')
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        console.dir(err, { depth: null });
+        console.log(`Error Type: ${typeof err}\n Error:${err}`);
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
