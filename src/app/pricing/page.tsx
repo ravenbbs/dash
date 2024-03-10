e58bcf3df476e4e2aa6622a6727e0b76af2112cd
Julio Condor <julioc3bgud@gmail.com>
2024-03-10 18:30:47 -0500

    Add details on pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,9 +1,66 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { PLANS } from "@/config/stripe";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const Page = () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
+
+  const pricingItems = [
+    {
+      plan: "Free",
+      tagline: "For small side projects.",
+      quota: 10,
+      features: [
+        {
+          text: "5 pages per PDF",
+          footnote: "The maximum amount of pages per PDF-file.",
+        },
+        {
+          text: "4MB file size limit",
+          footnote: "The maximum file size of a single PDF file.",
+        },
+        {
+          text: "Mobile-friendly interface",
+        },
+        {
+          text: "Higher-quality responses",
+          footnote: "Better algorithmic responses for enhanced content quality",
+          negative: true,
+        },
+        {
+          text: "Priority support",
+          negative: true,
+        },
+      ],
+    },
+    {
+      plan: "Pro",
+      tagline: "For larger projects with higher needs.",
+      quota: PLANS.find((p) => p.slug === "pro")!.quota,
+      features: [
+        {
+          text: "25 pages per PDF",
+          footnote: "The maximum amount of pages per PDF-file.",
+        },
+        {
+          text: "16MB file size limit",
+          footnote: "The maximum file size of a single PDF file.",
+        },
+        {
+          text: "Mobile-friendly interface",
+        },
+        {
+          text: "Higher-quality responses",
+          footnote: "Better algorithmic responses for enhanced content quality",
+        },
+        {
+          text: "Priority support",
+        },
+      ],
+    },
+  ];
 
   return (
     <>
@@ -15,10 +72,19 @@ const Page = () => {
             nuestro servicio.
           </p>
         </div>
-        <div className="pt-12 grid grid-cols-1 pag-10 lg:grid-cols-2"></div>
+        <div className="pt-12 grid grid-cols-1 pag-10 lg:grid-cols-2">
+          <TooltipProvider>
+            {pricingItems.map(({ plan, tagline, quota, features }) => {
+              const price =
+                PLANS.find((p) => p.slug === plan.toLocaleLowerCase())?.price
+                  .amount || 0;
+              return <div></div>;
+            })}
+          </TooltipProvider>
+        </div>
       </MaxWidthWrapper>
     </>
   );
 };
 
-export default Page
+export default Page;
