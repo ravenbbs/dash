2b81fd07d4d1370f258edf3bf68884ede50dc9bd
Julio Condor <julioc3bgud@gmail.com>
2024-03-11 22:50:35 -0500

    add utils absolute url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from 'next'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== 'undefined') return path
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}${path}`
+  return `http://localhost:${
+    process.env.PORT ?? 3000
+  }${path}`
+}
+
