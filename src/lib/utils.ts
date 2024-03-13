ecd729068f537a4fd6fe671e1f4fc048536dbd1f
Julio Condor <julioc3bgud@gmail.com>
2024-03-13 00:33:45 -0500

    fis error with git

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,46 @@ export function absoluteUrl(path: string) {
   }${path}`
 }
 
+export function constructMetadata({
+  title = "dash",
+  description = "Dash es un open-source software que permite chatear con tus PDFs fácilmente.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@ravenbbs"
+    },
+    icons,
+    metadataBase: new URL('https://dash-proyect.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
+}
