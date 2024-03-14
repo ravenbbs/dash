6eadcba82143836b8f079f6b202be69f74b4e0f4
Julio Condor <julioc3bgud@gmail.com>
2024-03-14 00:06:45 -0500

    Add navbar mobile

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,3 +1,18 @@
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Button } from './ui/button'
+import { Avatar, AvatarFallback } from './ui/avatar'
+import Image from 'next/image'
+import { Icons } from './Icons'
+import Link from 'next/link'
+import { Crown } from 'lucide-react'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
 
 interface UserAccountNavProps {
   email: string | undefined
@@ -5,11 +20,81 @@ interface UserAccountNavProps {
   imageUrl: string
 }
 
-const UserAccountNav = () => {
+const UserAccountNav = async ({
+  email,
+  imageUrl,
+  name,
+}: UserAccountNavProps) => {
+  const subscriptionPlan = await getUserSubscriptionPlan()
+
   return (
-    <div>
-      test
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        className='overflow-visible'>
+        <Button className='rounded-full h-8 w-8 aspect-square bg-slate-400'>
+          <Avatar className='relative w-8 h-8'>
+            {imageUrl ? (
+              <div className='relative aspect-square h-full w-full'>
+                <Image
+                  fill
+                  src={imageUrl}
+                  alt='profile picture'
+                  referrerPolicy='no-referrer'
+                />
+              </div>
+            ) : (
+              <AvatarFallback>
+                <span className='sr-only'>{name}</span>
+                <Icons.user className='h-4 w-4 text-zinc-900' />
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-0.5 leading-none'>
+            {name && (
+              <p className='font-medium text-sm text-black'>
+                {name}
+              </p>
+            )}
+            {email && (
+              <p className='w-[200px] truncate text-xs text-zinc-700'>
+                {email}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href='/dashboard'>Dashboard</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          {subscriptionPlan?.isSubscribed ? (
+            <Link href='/dashboard/billing'>
+              Administrar suscripción
+            </Link>
+          ) : (
+            <Link href='/pricing'>
+              Premium{' '}
+              <Crown className='text-blue-600 h-4 w-4 ml-1.5' />
+            </Link>
+          )}
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className='cursor-pointer'>
+            <LogoutLink>Cerrar sesión</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
