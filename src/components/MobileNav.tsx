3ae429275a6d65047e5e0f8b079abe0634daf8c0
Julio Condor <julioc3bgud@gmail.com>
2024-03-15 13:14:39 -0500

    fix sing out mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,6 +4,7 @@ import { ArrowRight, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
   const [isOpen, setOpen] = useState<boolean>(false)
@@ -82,11 +83,11 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
                 </li>
                 <li className='my-3 h-px w-full bg-gray-300' />
                 <li>
-                  <Link
+                  <LogoutLink
                     className='flex items-center w-full font-semibold'
-                    href='/sign-out'>
+                    >
                     Cerrar sesión
-                  </Link>
+                  </LogoutLink>
                 </li>
               </>
             )}
